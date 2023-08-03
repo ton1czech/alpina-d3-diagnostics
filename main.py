7ec520b74841c184321318aa36c2d7e530c93d42
ton1czech <tonyasek007@gmail.com>
2023-08-03 13:01:59 +0200

    🎁NEW: uphill run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
 
 folder = './data/2023-08-03/'
@@ -83,21 +84,59 @@ folder = './data/2023-08-03/'
 
 
 
+# def dyno():
+#     plt.style.use('dark_background')
+#     file_name = 'downhill.csv'
+#     csv_file = folder + file_name
+#     data = pd.read_csv(csv_file)
+
+#     fig, ax1 = plt.subplots(figsize=(18, 14))
+
+#     ax1.plot(data['Engine RPM (RPM)'], data['Engine Torque (N•m)'], label='Engine Torque', color='#e00b12', linestyle='-')
+#     ax1.set_xlabel('Engine RPM (RPM)')
+#     ax1.set_ylabel('Engine Torque (N•m)', color='#e00b12')
+
+#     ax2 = ax1.twinx()
+#     ax2.plot(data['Engine RPM (RPM)'], data['Engine Power (hp)'], label='Engine Power', color='#9309de', linestyle='-')
+#     ax2.set_ylabel('Engine Power (whp)', color='#9309de')
+
+#     max_power_idx = data['Engine Power (hp)'].idxmax()
+#     max_power_hp = data.loc[max_power_idx, 'Engine Power (hp)']
+#     max_power_rpm = data.loc[max_power_idx, 'Engine RPM (RPM)']
+
+#     max_torque_idx = data['Engine Torque (N•m)'].idxmax()
+#     max_torque = data.loc[max_torque_idx, 'Engine Torque (N•m)']
+#     max_torque_rpm = data.loc[max_torque_idx, 'Engine RPM (RPM)']
+
+#     max_boost = data['Boost (bar)'].max()
+
+#     label_text = f'{max_power_hp:.1f} whp @ {max_power_rpm} RPM\n{max_torque:.1f} N•m @ {max_torque_rpm} RPM\n{max_boost:.2f} bar'
+#     plt.text(0.02, 0.98, label_text, transform=ax2.transAxes, ha='left', va='top', fontsize=12)
+
+#     ax1.grid(True, axis='x', linewidth=0.5, color='#707070')
+
+#     plt.savefig(f"{folder}/dyno.png", bbox_inches='tight')
+
+#     plt.show()
+
+
+
 def dyno():
-    plt.style.use('dark_background')
-    file_name = 'downhill.csv'
+    file_name = 'uphill.csv'
     csv_file = folder + file_name
     data = pd.read_csv(csv_file)
 
-    fig, ax1 = plt.subplots(figsize=(18, 14))
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    ax1.plot(data['Engine RPM (RPM)'], data['Engine Torque (N•m)'], label='Engine Torque', color='#e00b12', linestyle='-')
-    ax1.set_xlabel('Engine RPM (RPM)')
-    ax1.set_ylabel('Engine Torque (N•m)', color='#e00b12')
+    fig.add_trace(go.Scatter(x=data['Engine RPM (RPM)'], y=data['Engine Torque (N•m)'],
+                             mode='lines', name='Engine Torque', line=dict(color='#e00b12')))
 
-    ax2 = ax1.twinx()
-    ax2.plot(data['Engine RPM (RPM)'], data['Engine Power (hp)'], label='Engine Power', color='#9309de', linestyle='-')
-    ax2.set_ylabel('Engine Power (whp)', color='#9309de')
+    fig.add_trace(go.Scatter(x=data['Engine RPM (RPM)'], y=data['Engine Power (hp)'],
+                             mode='lines', name='Engine Power', line=dict(color='#9309de')), secondary_y=True)
+
+    fig.update_xaxes(title_text='Engine RPM (RPM)')
+    fig.update_yaxes(title_text='Engine Torque (N•m)', color='#e00b12', secondary_y=False)
+    fig.update_yaxes(title_text='Engine Power (whp)', color='#9309de', secondary_y=True)
 
     max_power_idx = data['Engine Power (hp)'].idxmax()
     max_power_hp = data.loc[max_power_idx, 'Engine Power (hp)']
@@ -110,13 +149,29 @@ def dyno():
     max_boost = data['Boost (bar)'].max()
 
     label_text = f'{max_power_hp:.1f} whp @ {max_power_rpm} RPM\n{max_torque:.1f} N•m @ {max_torque_rpm} RPM\n{max_boost:.2f} bar'
-    plt.text(0.02, 0.98, label_text, transform=ax2.transAxes, ha='left', va='top', fontsize=12)
+    fig.add_annotation(
+        go.layout.Annotation(
+            text=label_text,
+            x=0.02,
+            y=0.98,
+            xref='paper',
+            yref='paper',
+            showarrow=False,
+            align='left',
+            valign='top',
+            font=dict(size=12)
+        )
+    )
 
-    ax1.grid(True, axis='x', linewidth=0.5, color='#707070')
+    fig.update_layout(
+        template='plotly_dark',
+        width=900,
+        height=700,
+        margin=dict(l=40, r=40, t=40, b=40),
+    )
 
-    plt.savefig(f"{folder}/dyno.png", bbox_inches='tight')
-
-    plt.show()
+    fig.write_image(f"{folder}/uphill.png")
+    fig.show()
 
 
 if __name__ == "__main__":
