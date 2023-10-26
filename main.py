9b71b48c43dc7fc9a4399dcbb81edb203335eb1a
ton1czech <tonyasek007@gmail.com>
2023-10-26 12:29:39 +0200

    🎁NEW: trying to make real time updating chart

--- main.py
+++ main.py
@@ -1,50 +1,102 @@
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import pandas as pd
+import plotly.express as px
+import threading
+import time
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
 
 folder = './data/2023-08-03/'
+file_name = 'uphill.csv'
+csv_file = folder + file_name
+data = pd.read_csv(csv_file)
 
-def pull():
-    plt.style.use('dark_background')
-    file_name = 'uphill.csv'
-    csv_file = folder + file_name
-    data = pd.read_csv(csv_file)
+app = dash.Dash(__name__)
 
-    fig, ax1 = plt.subplots(figsize=(18, 14))
+app.layout = html.Div([
+    dcc.Graph(id='live-graph'),
+    html.Div(id='boost-display'),
+    dcc.Interval(
+        id='interval-component',
+        interval=1*100,
+        n_intervals=0
+    )
+])
 
-    ax1.plot(data['Vehicle speed (km/h)'], data['Engine Torque (N•m)'], label='Engine Torque', color='#e00b12', linestyle='-')
-    ax1.set_xlabel('Vehicle Speed (km/h)')
-    ax1.set_ylabel('Engine Torque (N•m)', color='#e00b12')
 
-    ax2 = ax1.twinx()
-    ax2.plot(data['Vehicle speed (km/h)'], data['Engine Power (hp)'], label='Engine Power', color='#9309de', linestyle='-')
-    ax2.set_ylabel('Engine Power (whp)', color='#9309de')
+@app.callback([Output('live-graph', 'figure'),
+               Output('boost-display', 'children')],
+              [Input('interval-component', 'n_intervals')])
+def update_graph(n_intervals):
+    new_data = data.iloc[:n_intervals+1]
 
-    for x, y, rpm, bar in zip(data['Vehicle speed (km/h)'], data['Engine Power (hp)'], data['Engine RPM (RPM)'], data['Boost (bar)']):
-        ax2.scatter(x, y, color='#0aa6f5', s=20, alpha=0.5)
-        ax2.annotate(f'{rpm} RPM', (x, y), textcoords="offset points", xytext=(0, 9), ha='center', fontsize=9, color='#0aa6f5', alpha=0.7)
-        ax2.annotate(f'{bar} bar', (x, y), textcoords="offset points", xytext=(0, 24), ha='center', fontsize=12, color='#f5800a', alpha=0.7)
+    figure = {
+        'data': [
+            go.Scatter(x=new_data['Time (sec)'], y=new_data['Engine Power (hp)'],
+                       mode='lines+markers', name='Engine Power (hp)'),
+            go.Scatter(x=new_data['Time (sec)'], y=new_data['Engine Torque (N•m)'],
+                       mode='lines+markers', name='Engine Torque (N•m)'),
+        ],
+        'layout': {
+            'title': 'Dyno Chart',
+            'xaxis': {'title': 'Time (sec)'},
+            'yaxis': {'title': 'HP', 'side': 'left', 'range': [0, 280]},
+            'yaxis2': {'title': 'N•m', 'overlaying': 'y', 'side': 'right', 'range': [0, 560]},
+            'showlegend': True
+        }
+    }
 
-    max_power_idx = data['Engine Power (hp)'].idxmax()
-    max_power_hp = data.loc[max_power_idx, 'Engine Power (hp)']
-    max_power_rpm = data.loc[max_power_idx, 'Engine RPM (RPM)']
+    latest_boost = new_data['Boost (bar)'].iloc[-1]
 
-    max_torque_idx = data['Engine Torque (N•m)'].idxmax()
-    max_torque = data.loc[max_torque_idx, 'Engine Torque (N•m)']
-    max_torque_rpm = data.loc[max_torque_idx, 'Engine RPM (RPM)']
+    boost_display = f'Boost: {latest_boost} bar'
 
-    max_boost = data['Boost (bar)'].max()
+    return figure, boost_display
 
-    label_text = f'{max_power_hp:.1f} whp @ {max_power_rpm} RPM\n{max_torque:.1f} N•m @ {max_torque_rpm} RPM\n{max_boost:.2f} bar'
-    plt.text(0.02, 0.98, label_text, transform=ax2.transAxes, ha='left', va='top', fontsize=12)
 
-    ax1.grid(True, axis='x', linewidth=0.5, color='#707070')
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
-    plt.savefig(f"{folder}/uphill.png", bbox_inches='tight')
+# def pull():
+#     plt.style.use('dark_background')
+#     file_name = 'uphill.csv'
+#     csv_file = folder + file_name
+#     data = pd.read_csv(csv_file)
 
-    plt.show()
+#     fig, ax1 = plt.subplots(figsize=(18, 14))
 
+#     ax1.plot(data['Vehicle speed (km/h)'], data['Engine Torque (N•m)'], label='Engine Torque', color='#e00b12', linestyle='-')
+#     ax1.set_xlabel('Vehicle Speed (km/h)')
+#     ax1.set_ylabel('Engine Torque (N•m)', color='#e00b12')
+
+#     ax2 = ax1.twinx()
+#     ax2.plot(data['Vehicle speed (km/h)'], data['Engine Power (hp)'], label='Engine Power', color='#9309de', linestyle='-')
+#     ax2.set_ylabel('Engine Power (whp)', color='#9309de')
+
+#     for x, y, rpm, bar in zip(data['Vehicle speed (km/h)'], data['Engine Power (hp)'], data['Engine RPM (RPM)'], data['Boost (bar)']):
+#         ax2.scatter(x, y, color='#0aa6f5', s=20, alpha=0.5)
+#         ax2.annotate(f'{rpm} RPM', (x, y), textcoords="offset points", xytext=(0, 9), ha='center', fontsize=9, color='#0aa6f5', alpha=0.7)
+#         ax2.annotate(f'{bar} bar', (x, y), textcoords="offset points", xytext=(0, 24), ha='center', fontsize=12, color='#f5800a', alpha=0.7)
+
+#     max_power_idx = data['Engine Power (hp)'].idxmax()
+#     max_power_hp = data.loc[max_power_idx, 'Engine Power (hp)']
+#     max_power_rpm = data.loc[max_power_idx, 'Engine RPM (RPM)']
+
+#     max_torque_idx = data['Engine Torque (N•m)'].idxmax()
+#     max_torque = data.loc[max_torque_idx, 'Engine Torque (N•m)']
+#     max_torque_rpm = data.loc[max_torque_idx, 'Engine RPM (RPM)']
+
+#     max_boost = data['Boost (bar)'].max()
+
+#     label_text = f'{max_power_hp:.1f} whp @ {max_power_rpm} RPM\n{max_torque:.1f} N•m @ {max_torque_rpm} RPM\n{max_boost:.2f} bar'
+#     plt.text(0.02, 0.98, label_text, transform=ax2.transAxes, ha='left', va='top', fontsize=12)
+
+#     ax1.grid(True, axis='x', linewidth=0.5, color='#707070')
+
+#     plt.savefig(f"{folder}/uphill.png", bbox_inches='tight')
+
+#     plt.show()
 
 
 # def trip():
@@ -83,7 +135,6 @@ def pull():
 #     plt.show()
 
 
-
 # def dyno():
 #     plt.style.use('dark_background')
 #     file_name = 'downhill.csv'
@@ -117,7 +168,6 @@ def pull():
 #     plt.savefig(f"{folder}/dyno.png", bbox_inches='tight')
 
 #     plt.show()
-
 
 
 # def dyno():
@@ -173,5 +223,5 @@ def pull():
 #     fig.show()
 
 
-if __name__ == "__main__":
-    pull()
+# if __name__ == "__main__":
+#     pull()
